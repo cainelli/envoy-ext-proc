81c3fbafe4c178135ba5a7160d131fa5535dd4b5
Fernando Cainelli <fernando.cainelli@getyourguide.com>
2024-02-13 18:56:50 -0300

    update docs and helper methods

--- pkg/service/processor/request.go
+++ pkg/service/processor/request.go
@@ -2,27 +2,34 @@ package processor
 
 import (
 	"net/url"
+	"strconv"
 	"strings"
 
 	corev3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	extproc "github.com/envoyproxy/go-control-plane/envoy/service/ext_proc/v3"
 )
 
+// Request stores the context between the different gRPC messages received from Envoy and it is used to store the request headers, response headers, and other information about the request.
+// The Process method should be called on every message received from Envoy in order to update the request object.
+// Note that the request object is not thread-safe and should not be shared between goroutines.
 type Request struct {
 	scheme          string
 	authority       string
 	method          string
 	url             *url.URL
 	requestId       string
+	status          int
 	requestHeaders  *extproc.ProcessingRequest_RequestHeaders
 	responseHeaders *extproc.ProcessingRequest_ResponseHeaders
-	commonResponse  *extproc.CommonResponse
 }
 
+// RequestHeaders returns the HTTP request headers. All header keys will be lower-cased.
 func (r *Request) RequestHeaders() []*corev3.HeaderValue {
 	return r.requestHeaders.RequestHeaders.GetHeaders().GetHeaders()
 }
 
+// GetRequestHeader returns the value of the header with the given key. If the header is not found, it returns an empty string.
+// If the header has multiple values, it returns the first one. If you need all the values, use RequestHeaders()
 func (r *Request) GetRequestHeader(key string) string {
 	for _, header := range r.RequestHeaders() {
 		if header.Key == key {
@@ -35,10 +42,13 @@ func (r *Request) GetRequestHeader(key string) string {
 	return ""
 }
 
+// ResponseHeaders returns the HTTP response headers. All header keys will be lower-cased.
 func (r *Request) ResponseHeaders() []*corev3.HeaderValue {
 	return r.responseHeaders.ResponseHeaders.GetHeaders().GetHeaders()
 }
 
+// GetResponseHeader returns the value of the header with the given key. If the header is not found, it returns an empty string.
+// If the header has multiple values, it returns the first one. If you need all the values, use ResponseHeaders()
 func (r *Request) GetResponseHeader(key string) string {
 	for _, header := range r.ResponseHeaders() {
 		if header.Key == key {
@@ -51,10 +61,38 @@ func (r *Request) GetResponseHeader(key string) string {
 	return ""
 }
 
-func (r *Request) CommonResponse() *extproc.CommonResponse {
-	return r.commonResponse
+// Scheme returns the scheme of the request (http or https)
+func (r *Request) Scheme() string {
+	return r.scheme
 }
 
+// Authority returns the authority of the request
+func (r *Request) Authority() string {
+	return r.authority
+}
+
+// Method returns the method of the request (GET, POST, PUT, etc)
+func (r *Request) Method() string {
+	return r.method
+}
+
+// URL returns the URL of the request
+func (r *Request) URL() *url.URL {
+	return r.url
+}
+
+// RequestID returns the request ID of the request
+func (r *Request) RequestID() string {
+	return r.requestId
+}
+
+// Status returns the status of the response
+func (r *Request) Status() int {
+	return r.status
+}
+
+// Process processes the given message and updates the request object accordingly
+// It should be called on every message received from Envoy
 func (r *Request) Process(message any) {
 	switch msg := any(message).(type) {
 	case *extproc.ProcessingRequest_RequestHeaders:
@@ -69,9 +107,7 @@ func (r *Request) Process(message any) {
 	case r.responseHeaders == nil:
 		r.responseHeaders = &extproc.ProcessingRequest_ResponseHeaders{}
 	}
-}
 
-func (r *Request) init() {
 	var err error
 	if r.scheme == "" {
 		r.scheme = r.GetRequestHeader(":scheme")
@@ -94,5 +130,9 @@ func (r *Request) init() {
 				RawQuery: strings.Split(r.GetRequestHeader(":path"), "?")[1],
 			}
 		}
+	}
+	if r.status == 0 {
+		status, _ := strconv.Atoi(r.GetResponseHeader(":status"))
+		r.status = status
 	}
 }
--- pkg/service/processor/response.go
+++ pkg/service/processor/response.go
@@ -8,6 +8,8 @@ import (
 	"github.com/golang/protobuf/ptypes/wrappers"
 )
 
+// CommonResponseWriter is a wraper on top of extproc.CommonResponse
+// It provides a fluent API to mutate the request and response headers and body
 type CommonResponseWriter struct {
 	commonResponse *extproc.CommonResponse
 }
@@ -23,6 +25,7 @@ func NewCommonResponseWriter() *CommonResponseWriter {
 	return crw
 }
 
+// HeaderAction sets a header with the given key and value and the given append action
 func (crw *CommonResponseWriter) HeaderAction(key string, value string, appendAction corev3.HeaderValueOption_HeaderAppendAction) *CommonResponseWriter {
 	// FIXME: This is not the documented behavior but it seems to be the only way to append a header.
 	var append *wrappers.BoolValue
@@ -44,14 +47,33 @@ func (crw *CommonResponseWriter) HeaderAction(key string, value string, appendAc
 	})
 }
 
+// GetMutatedHeader returns the value of the header that was mutated by one of the processors.
+// If the header has multiple values, it returns the first one. If you need all the values, use the underlying CommonResponse().HeaderMutation.SetHeaders
+func (crw *CommonResponseWriter) GetMutatedHeader(key string) string {
+	for _, h := range crw.commonResponse.GetHeaderMutation().GetSetHeaders() {
+		if h.GetHeader().GetKey() == key {
+			if h.GetHeader().GetValue() != "" {
+				return h.GetHeader().GetValue()
+			}
+			return string(h.GetHeader().GetRawValue())
+		}
+	}
+	return ""
+}
+
+// HeaderSet sets a header with the given key and value using the OVERWRITE_IF_EXISTS_OR_ADD action
+// This action will overwrite the specified value by discarding any existing values if the header already exists. If the header doesn't exist then this will add the header with specified key and value.
 func (crw *CommonResponseWriter) HeaderSet(key string, value string) *CommonResponseWriter {
 	return crw.HeaderAction(key, value, corev3.HeaderValueOption_OVERWRITE_IF_EXISTS_OR_ADD)
 }
 
+// HeaderAppend appends a header with the given key and value using the APPEND_IF_EXISTS_OR_ADD action
+// This action will append the specified value to the existing values if the header already exists. If the header doesn't exist then this will add the header with specified key and value.
 func (crw *CommonResponseWriter) HeaderAppend(key string, value string) *CommonResponseWriter {
 	return crw.HeaderAction(key, value, corev3.HeaderValueOption_APPEND_IF_EXISTS_OR_ADD)
 }
 
+// RemoveHeaders removes these HTTP headers. Attempts to remove system headers -- any header starting with “:“, plus “host“ -- will be ignored.
 func (crw *CommonResponseWriter) RemoveHeaders(headers ...string) *CommonResponseWriter {
 	for _, h := range headers {
 		if slices.Contains(crw.commonResponse.HeaderMutation.RemoveHeaders, h) {
@@ -62,21 +84,28 @@ func (crw *CommonResponseWriter) RemoveHeaders(headers ...string) *CommonRespons
 	return crw
 }
 
+// SetStatus sets the status of the GRPC response.
+// If set, provide additional direction on how the Envoy proxy should handle the rest of the HTTP filter chain.
 func (crw *CommonResponseWriter) SetStatus(status extproc.CommonResponse_ResponseStatus) *CommonResponseWriter {
 	crw.commonResponse.Status = status
 	return crw
 }
 
+// ClearRouteCache clears the route cache for the current client request. This is necessary if the remote server modified headers that are used to calculate the route. This field is ignored in the response direction.
 func (crw *CommonResponseWriter) ClearRouteCache(clear bool) *CommonResponseWriter {
 	crw.commonResponse.ClearRouteCache = clear
 	return crw
 }
 
+// Replace the body of the last message sent to the remote server on this stream.
+// If responding to an HttpBody request, simply replace or clear the body chunk that was sent with that request.
+// Body mutations may take effect in response either to “header“ or “body“ messages. When it is in response to “header“ messages, it only take effect if the :ref:`status <envoy_v3_api_field_service.ext_proc.v3.CommonResponse.status>` is set to CONTINUE_AND_REPLACE.
 func (crw *CommonResponseWriter) BodyMutation(m *extproc.BodyMutation) *CommonResponseWriter {
 	crw.commonResponse.BodyMutation = m
 	return crw
 }
 
+// CommonResponse returns the underlying extproc.CommonResponse
 func (crw *CommonResponseWriter) CommonResponse() *extproc.CommonResponse {
 	return crw.commonResponse
 }
